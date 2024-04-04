fbdcf1b98afa85b99cb31b0b9a8a6b73388a1175
jairmart <jair.martinez@salud-digna.org>
2024-04-03 22:02:58 -0600

    productos

--- app/contacto/page.jsx
+++ app/contacto/page.jsx
@@ -15,11 +15,11 @@ function contacto() {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           <div>
             <h3 className="text-xl font-medium text-gray-600 mb-4">Información de contacto</h3>
-            <p className="text-gray-600 text-3xl">
-              <span className='text-[#0C71C3] '>Teléfono:</span> <span className=''>+52 442 642 3149</span><br />
-              <span className='text-[#0C71C3] '>Movil:</span> <span className=''>+52 442-561-9371</span><br />
-              <span className='text-[#0C71C3] '>Email:</span> <span className=''>info@endomedicsqro.com</span><br />
-              <span className='text-[#0C71C3] '>Dirección:</span> <span className=''>Calle Principal #123, Ciudad</span>
+            <p className="text-gray-600 text-xl">
+              <span className='text-[#0C71C3] '>Teléfono:</span> <span className='text-md'>+52 442 642 3149</span><br />
+              <span className='text-[#0C71C3] '>Movil:</span> <span className='text-md'>+52 442-561-9371</span><br />
+              <span className='text-[#0C71C3] '>Email:</span> <span className='text-md'>info@endomedicsqro.com</span><br />
+              <span className='text-[#0C71C3] '>Dirección:</span> <span className='text-md'>Calle Principal #123, Ciudad</span>
             </p>
           </div>
           <div>
--- app/productos/page.jsx
+++ app/productos/page.jsx
@@ -1,9 +1,10 @@
+import ImageProductos from '@/components/Productos/ImageProductos'
 import React from 'react'
 
 function productos() {
   return (
     <div id='productos'>
-      Productos
+      <ImageProductos/>
     </div>
   )
 }
